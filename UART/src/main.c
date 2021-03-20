71723acb021ed7b97bbd2b596f23322b94905adc
Nathan Bunnell <nbunnell776@gmail.com>
2021-03-20 13:10:38 -0600

    UART echo functioning

--- UART/src/main.c
+++ UART/src/main.c
@@ -8,62 +8,31 @@
 //  PA3 - USART2 RX
 //
 
-#include "../device_headers/stm32f411xe.h"  // Device headers
+#include "main.h"                           // Function defs and link to headers
 
 void USART2_init(void);                     // Init func dec
 void USART2_write(int ch);                  // Write func dec
-void USART2_read(void);                     // Read func dec
+char USART2_read(void);                     // Read func dec
 
 void delay_ms(uint16_t t);                  // Simple delay func dec
 
 int main(void)
 {
+    char c;                                // RX data to echo
     USART2_init();                          // Initialize USART2
     
     while(1)
     {
-        USART2_write('0');                  // Echo back value
-        USART2_write('x');                  // Echo back value
-        USART2_write('D');                  // Echo back value
-        USART2_write('E');                  // Echo back value
-        USART2_write('A');                  // Echo back value
-        USART2_write('D');                  // Echo back value
-        USART2_write('B');                  // Echo back value
-        USART2_write('E');                  // Echo back value
-        USART2_write('E');                  // Echo back value
-        USART2_write('F');                  // Echo back value
-        USART2_write(13);                   // Echo a carriage return
-        USART2_write(10);                   // Echo a line feed
+        c = USART2_read();                  // Get character
+
+        if (c == '\r')                      // I think this is a Unix style line 
+        {                                   // ending issue on my dev machine. 
+            USART2_write(10);               // To be further researched...
+            USART2_write(13);
+        }
+        else{}
         
-        delay_ms(500);                      // Short delay
+        USART2_write(c);                    // Echo on serial port
     }
-}
 
-void USART2_init(void)
-{
-    RCC->AHB1ENR |= 1;                      // Enable GPIOA clock
-    RCC->APB1ENR |= 0x20000;                // Enable USART2 clock
-
-    GPIOA->AFR[0] &= ~0x0F00;               // Cfg alt func register
-    GPIOA->AFR[0] |= 0x0700;
-    GPIOA->MODER &= ~0x0030;
-    GPIOA->MODER |= 0x0020;                  // Enable alt func register
-
-    USART2->BRR = 0x0683;                    // 9600 baud @ 16MHz
-    USART2->CR1 = 0x0008;                    // Enable Tx, 8 data bits
-    USART2->CR2 = 0x0000;                    // 1 stop bit
-    USART2->CR3 = 0x0000;                    // No flow control
-    USART2->CR1 |= 0x2000;                   // Enable USART2
-}
-
-void USART2_write(int ch)
-{
-    while (!(USART2->SR & 0x0080)) {}        // Wait until TX buffer is empty
-    USART2->DR = (ch & 0xFF);
-}
-
-void delay_ms(uint16_t t)                   // Simple delay func def
-{
-    for (; t > 0; t--)
-        for (int i = 0; i < 3195; i++) ;    // 3195 scales msec to 16MHz clock
 }
--- UART/src/main.h
+++ UART/src/main.h
@@ -1,9 +1,50 @@
-#ifndef _VVC_MAIN_H
-#define _VVC_MAIN_H
+#ifndef _UART_MAIN_H
+#define _UART_MAIN_H
 
-#include "../device_headers/stm32f411xe.h"
+#include "../device_headers/stm32f411xe.h"  // Device headers
 
-// Defines GPIO mapping for buttons and LED
-#define LED_PIN         (13)                // Port C Pin 13 is onboard LED pin
+void USART2_init(void)
+{
+    // Clock enable
+    RCC->AHB1ENR |= 1;                      // Enable GPIOA clock
+    RCC->APB1ENR |= 0x20000;                // Enable USART2 clock
+
+    // Cfg for PA2/ USART2 TX
+    GPIOA->AFR[0] &= ~0x0F00;               // Cfg alt func register
+    GPIOA->AFR[0] |= 0x0700;
+    GPIOA->MODER &= ~0x0030;                // Enable alt func register
+    GPIOA->MODER |= 0x0020;                 
+
+    // Cfg for PA3/ USART3 TX
+    GPIOA->AFR[0] &= ~0xF000;               // Cfg alt func register
+    GPIOA->AFR[0] |= 0x7000;
+    GPIOA->MODER &= ~0x00C0;                // Enable alt func register
+    GPIOA->MODER |= 0x0080;                 
+
+    // USART functional cfg
+    USART2->BRR = 0x0683;                   // 9600 baud @ 16MHz w/ oversampling by 16
+    USART2->CR1 = 0x00AC;                   // Enable Tx, RX, 8 data bits
+    USART2->CR2 = 0x0000;                   // 1 stop bit
+    USART2->CR3 = 0x0000;                   // No flow control
+    USART2->CR1 |= 0x2000;                  // Enable USART2
+}
+
+void USART2_write(int ch)
+{
+    while (!(USART2->SR & 0x0080)) {}       // Wait until TX buffer is empty
+    USART2->DR = (ch & 0xFF);
+}
+
+char USART2_read(void)
+{
+    while (!(USART2->SR & 0x0020)) {}       // Wait until RX buffer is not empty
+    return USART2->DR;
+}
+
+void delay_ms(uint16_t t)                   // Simple delay func def
+{
+    for (; t > 0; t--)
+        for (int i = 0; i < 3195; i++) ;    // 3195 scales msec to 16MHz clock
+}
 
 #endif
